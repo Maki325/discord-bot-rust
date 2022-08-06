d83f55ea244b087d24dc0e7323a1c2698aa945af
Marko <markoni521@gmail.com>
2022-08-06 03:05:46 +0200

    Added `selector roles` command
    We can pass the container struct around, FINALLY
    We added a save option to the container, so now we actually serialize and DESERIALIZE data :O

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::fs;
 use dotenv::dotenv;
 use std::time::Duration;
 use std::collections::HashMap;
+use std::sync::{Mutex};
+use lazy_static::lazy_static;
 
 use serde::{Serialize, Deserialize};
 
@@ -13,31 +15,41 @@ use serenity::framework::standard::{StandardFramework, CommandResult};
 use serenity::model::gateway::Ready;
 use serenity::model::prelude::Message;
 use serenity::model::guild::{Emoji, Role};
+use serenity::model::channel::ReactionType::{Custom, Unicode};
+use serenity::model::id::EmojiId;
 use serenity::builder::{CreateActionRow, CreateSelectMenu, CreateSelectMenuOption};
 use tracing::{info};
 // use tracing_subscriber::FmtSubscriber;
 
+lazy_static! {
+    static ref BOT_DATA: Mutex<Container> = Mutex::new(Container::new("save_data.json")/*Container {
+        guilds: HashMap::new(),
+        messages: HashMap::new(),
+    }*/);
+}
+
 #[group]
-#[commands(ping)]
+#[commands(ping, selector)]
 struct General;
 
-struct Handler {
-    save_data: Container,
-}
+struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-
+    
     async fn ready(&self, ctx: Context, ready: Ready) {
         // Log at the INFO level. This is a macro from the `tracing` crate.
         println!("{} is connected!", ready.user.name);
         info!("{} is connected!", ready.user.name);
 
-        for (k, v) in &self.save_data.messages {
-            println!("Key: {}", k);
-            println!("Value: {}", serde_json::to_string(&v).expect("Couldn't json the value!"));
+        {
+            let container = BOT_DATA.lock().unwrap();
+            for (k, v) in &container.messages {
+                println!("Key: {}", k);
+                println!("Value: {}", serde_json::to_string(&v).expect("Couldn't json the value!"));
+            }
         }
-
+        
         for guild_info in ctx.http.get_guilds(None, None).await.expect("Get guilds error!") {
             println!("Guild id: {}", guild_info.id);
 
@@ -49,40 +61,83 @@ impl EventHandler for Handler {
                 roles: partial_guild.roles.values().cloned().collect::<Vec<Role>>(),
             };
             
+            let mut container = BOT_DATA.lock().unwrap();
+
             // TODO: Figure out how to use save_data, maybe as a reference
             // Because self is NOT mutable sadly
-            &self.save_data.guilds.insert(guild.id.to_string(), Guild {
+            container.guilds.insert(guild.id.to_string(), Guild {
                 id: partial_guild.id.0,
                 emojis: partial_guild.emojis.values().cloned().collect::<Vec<Emoji>>(),
                 roles: partial_guild.roles.values().cloned().collect::<Vec<Role>>(),
             });
         }
+
+        {
+            let container = BOT_DATA.lock().unwrap();
+
+            for (k, v) in &container.guilds {
+                println!("Key: {}", k);
+                println!("Value: {}", serde_json::to_string(&v).expect("Couldn't json the value!"));
+            }
+        }
     }
 
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Container {
+    path: Option<String>,
     guilds: HashMap<String, Guild>,
     messages: HashMap<String, MessageActions>,
 }
 
-#[derive(Serialize, Deserialize)]
+impl Container {
+    fn new(path: &str) -> Container {
+        let json_data = fs::read_to_string(path).expect("Couldn't read the file!");
+        let mut container: Container = serde_json::from_str(&json_data).expect("Couldn't json the file!");
+        container.path = Some(path.to_string());
+
+        return container;
+    }
+
+    fn save(&self) {
+        fs::write(&self.path.as_ref().expect("No path on container!"), serde_json::to_string(self).expect("Couldn't turn Container into json!"));
+    }
+
+    fn get_guild_role_by_name<S: AsRef<str>>(&self, guild_id: S, role_name: S) -> Option<&Role> {
+        match self.guilds.get(guild_id.as_ref()) {
+            None => None,
+            Some(guild) => guild.get_role_by_name(role_name),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 struct Guild {
     id: u64,
     emojis: Vec<Emoji>,
     roles: Vec<Role>,
 }
 
-#[derive(Serialize, Deserialize)]
+impl Guild {
+    fn get_role_by_name<S: AsRef<str>>(&self, role_name: S) -> Option<&Role> {
+        let name = role_name.as_ref();
+        for role in &self.roles {
+            if role.name.eq(name) { return Some(role); }
+        }
+        return None;
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 struct MessageActions {
     id: u64,
     roles: Vec<EmojiRoleMapping>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct EmojiRoleMapping {
-    emoji: u64,
+    emoji: String,
     role: u64,
 }
 
@@ -95,17 +150,20 @@ async fn main() {
         .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
-    let json_data = fs::read_to_string("save_data.json").expect("Couldn't read the file!");
-    let container: Container = serde_json::from_str(&json_data).expect("Couldn't json the file!");
+    
+    /*let mut c = BOT_DATA.lock().unwrap();
+    c.guilds = container.guilds.clone();
+    c.messages = container.messages.clone();*/
 
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
         
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILD_MESSAGE_REACTIONS;
     let mut client = Client::builder(token, intents)
-        .event_handler(Handler {save_data: container})
+        .event_handler(Handler)
         .framework(framework)
         .await
         .expect("Error creating client");
@@ -115,6 +173,105 @@ async fn main() {
         println!("An error occurred while running the client: {:?}", why);
     }
     println!("The bot started");
+}
+
+#[command]
+async fn roles(ctx: &Context, msg: &Message) -> CommandResult {
+    // ~selector roles (:some_emoji_idk: | role name) (:some_other_emoji: | other role name)
+    println!("Roles: {}", msg.content);
+
+    let content = msg.content.replace("~selector roles", "").trim().to_string();
+    if content.is_empty() {
+        msg.reply(&ctx, "Usage: ~selector roles (:some_emoji_idk: | role name) (:some_other_emoji: | other role name)").await?;
+        return Ok(());
+    }
+    let container = BOT_DATA.lock().unwrap().clone();
+    let mut message_actions = MessageActions {id: 0, roles: Vec::new()};
+    let mut message_parts: Vec<String> = Vec::new();
+
+    let items: Vec<&str> = content.split(") (").collect(); //.iter().map(|s| s.split("|"));
+    for mut item in items {
+        item = item.trim();
+
+        let string_item: String = item.to_string().replace("(", "").replace(")", "")
+            .trim().to_string();
+        /*if item.starts_with("(") && item.ends_with(")") {
+            println!("Option 1! take: '{}'", item.len());
+            string_item = item.chars().skip(1).take(item.len() - 3).collect()
+        } else if item.ends_with(")") {
+            println!("Option 2!");
+            string_item = item.chars().take(item.len() - 1).collect()
+        } else if item.starts_with("(") {
+            println!("Option 3!");
+            string_item = item.chars().skip(1).collect();
+        } else {
+            println!("Option 4!");
+            string_item = item.chars().collect();
+        }
+        string_item = string_item.trim().to_string();*/
+        println!("Item: {}", item);
+        println!("String item: {}", string_item);
+
+        let parts: Vec<&str> = string_item.split("|").collect();
+        let (emoji, role) = (parts[0].trim(), parts[1].trim());
+
+        // msg.channel_id.send_message(ctx, "").await.expect("Expected the message!");
+        println!("Role: '{}'", role);
+        let role_id: u64 = match container.get_guild_role_by_name(msg.guild_id.unwrap().0.to_string(), role.to_string()) {
+            None => {
+                msg.reply(&ctx, "No role with that name!").await.unwrap();
+                return Ok(());
+            },
+            Some(role) => role.id.0,
+        };
+        message_actions.roles.push(EmojiRoleMapping {
+            role: role_id,
+            emoji: emoji.to_string(),
+        });
+
+        // <@&739501493639577653>
+        message_parts.push(format!("You can get <@&{}> if you react with {}", role_id, emoji));
+    }
+
+    println!("message_actions: {}", serde_json::to_string(&message_actions).expect("Couldn't json the value!"));
+
+    let message = msg.channel_id.say(&ctx, message_parts.join("\n")).await.unwrap();
+    for mapping in &message_actions.roles {
+        let emoji = &mapping.emoji;
+        println!("Emoji: {}", emoji);
+        if emoji.starts_with("<") {
+            let parts: &Vec<&str> = &mapping.emoji.split(":").collect();
+            let name = parts[1].to_string();
+            let id = parts[2].replace(">", "").to_string().parse::<u64>().unwrap();
+            println!("Emoji Id: {}", id);
+            message.react(&ctx, Custom{animated: false, id: EmojiId(id), name: Some(name)}).await.expect("Error react!");
+        } else {
+            message.react(&ctx, Unicode(emoji.to_string())).await.unwrap();
+        }
+    }
+    {
+        let id: u64 = message.id.0;
+        message_actions.id = id;
+        let mut container = BOT_DATA.lock().unwrap();
+        container.messages.insert(id.to_string(), message_actions);
+        container.save();
+    }
+
+    msg.reply(&ctx, "Something").await?;
+
+    Ok(())
+}
+
+#[command]
+#[sub_commands(roles)]
+async fn selector(ctx: &Context, msg: &Message) -> CommandResult {
+    // ~selector
+    msg.reply(&ctx, r#"Usage:
+        - selector roles (:some_emoji_idk: | role name) (:some_other_emoji: | other role name)
+    "#).await?;
+    println!("selector {}", msg.content);
+
+    Ok(())
 }
 
 fn create_option(label: &str, value: &str) -> CreateSelectMenuOption {
